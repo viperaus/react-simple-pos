07060103fcdd49bf966a345bb9421853528c8f71
Troy <viperaus@gmail.com>
2022-08-03 00:08:47 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##Demo Server Endpoint:
+
+Create a .env file with the following content
+
+```
+VITE_APP_API_SERVER_DEV=https://react-simple-pos-server.vercel.app
+VITE_APP_API_SERVER_PROD=https://react-simple-pos-server.vercel.app
+```
---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route, Outlet } from 'react-router-dom'
+
+import LoginPage from './Pages/Login'
+import PosPage from './Pages/Pos'
+import SettingsPage from './Pages/Settings'
+import LogoutPage from './Pages/Logout'
+
+/**
+ * TODO:
+ * Handle authentication in a better way (see: https://reactrouter.com/docs/en/v6/examples/auth)
+ *
+ * When setting route to /pos or /settings and not authenticated, we should redirect to /login and not just show the login form
+ *
+ */
+
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false)
+
+  useEffect(() => {
+    const prefillData = localStorage.getItem('prefillData')
+    if (prefillData !== null) {
+      setAuthenticated(true)
+    } else {
+      setAuthenticated(false)
+    }
+  }, [])
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage setAuthenticated={setAuthenticated} authenticated={authenticated} />} />
+        <Route path="/pos" element={authenticated ? <PosPage /> : <LoginPage setAuthenticated={setAuthenticated} authenticated={authenticated} />} />
+        <Route path="/settings" element={authenticated ? <SettingsPage /> : <LoginPage setAuthenticated={setAuthenticated} authenticated={authenticated} />} />
+        <Route path="/logout" element={<LogoutPage setAuthenticated={setAuthenticated} authenticated={authenticated} />} />
+        <Route path="*" element={<LoginPage setAuthenticated={setAuthenticated} authenticated={authenticated} />} />
+      </Routes>
+      <Outlet />
+    </>
+  )
+}
+
+export default App
