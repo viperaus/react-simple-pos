60e4ab846e5a9eeb2ae6ab003631cb69581db944
Troy <viperaus@gmail.com>
2022-08-03 18:09:10 +1000

    pretty up the ui

--- src/Components/LogoutForm/index.jsx
+++ src/Components/LogoutForm/index.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function LogoutForm() {
   return (
-    <>
-      <p>You have been logged out</p>
-      <p>
-        Return to the <Link to="/login">Log In</Link> page
-      </p>
-    </>
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 text-white text-center ">
+        <p>You have been logged out</p>
+        <p>
+          Return to the <Link to="/login">Log In</Link> page
+        </p>
+      </div>
+    </div>
   )
 }
