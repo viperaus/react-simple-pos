9020df2fb43a8872b9aee6fe6ce25b0397a3b3a8
Troy <viperaus@gmail.com>
2022-08-04 20:57:08 +1000

    Logic to add items to current sale and to clear the sale

--- src/Pages/Pos/index.jsx
+++ src/Pages/Pos/index.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ScreenLeft from '../../Components/ScreenLeft'
 import ScreenRight from '../../Components/ScreenRight'
+
+/*
+ TODO
+  * Use a library to handle passing functions from this top level down all levels until it reaches the buttons
+*/
 
 const PosPage = () => {
   const [currentSaleItems, setCurrentSaleItems] = useState([])
 
+  const addItemIntoCurrentSale = (plu, description, price) => {
+    let combinedItems = [...currentSaleItems, { plu, description, price }]
+    setCurrentSaleItems(combinedItems)
+    localStorage.setItem('current_sale', JSON.stringify(combinedItems))
+  }
+
+  const clearCurrentSale = () => {
+    setCurrentSaleItems([])
+    localStorage.setItem('current_sale', JSON.stringify([]))
+  }
+
+  const saleFunctions = {
+    sellItem: addItemIntoCurrentSale,
+    clearSale: clearCurrentSale,
+  }
+
+  useEffect(() => {
+    let storedItems = [...JSON.parse(localStorage.getItem('current_sale'))]
+    if (storedItems.length > 0) {
+      setCurrentSaleItems(storedItems)
+    }
+  }, [])
+
   return (
-    <div className="container">
+    <div className="w-screen">
       <div className="flex flex-row h-screen">
         <ScreenLeft currentSaleItems={currentSaleItems} />
-        <ScreenRight />
+        <ScreenRight functions={saleFunctions} />
       </div>
     </div>
   )
