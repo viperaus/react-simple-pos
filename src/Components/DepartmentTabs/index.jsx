900e516731174b97298659c303330fe022c7caaa
Troy <viperaus@gmail.com>
2022-08-04 20:52:31 +1000

    Build tab component to cycle through departments

---
+++ src/Components/DepartmentTabs/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { departments } from '../../MockData/products'
+
+import DepartmentTab from './DepartmentTab'
+import DepartmentTabContent from './DepartmentTabContent'
+
+const DepartmentTabs = ({ functions }) => {
+  const [selectedDepartment, setSelectedDepartment] = useState(departments[0].description)
+
+  const style = { ul: 'nav nav-tabs flex flex-row md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4 bg-slate-100' }
+
+  return (
+    <>
+      <ul className={style.ul} id="tabs-tab" role="tablist">
+        {departments.map((item) => {
+          return <DepartmentTab {...item} selectedDepartment={selectedDepartment} setSelectedDepartment={setSelectedDepartment} />
+        })}
+      </ul>
+      <div className="tab-content" id="tabs-tabContent">
+        {departments.map((item) => {
+          return <DepartmentTabContent {...item} selectedDepartment={selectedDepartment} functions={functions} />
+        })}
+      </div>
+    </>
+  )
+}
+
+export default DepartmentTabs
