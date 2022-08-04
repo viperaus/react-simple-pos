1e931911268406c1aa258f43c425c81ded3d4f4e
Troy <viperaus@gmail.com>
2022-08-04 21:27:09 +1000

    Handle product images on buttons

--- src/Components/ProductButton/index.jsx
+++ src/Components/ProductButton/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductButton = ({ plu, description, price, onStop, functions }) => {
+const ProductButton = ({ plu, description, price, onStop, img, functions }) => {
   const defaultStyle =
     'inline-block mx-1 my-1 px-6 py-2.5  font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out'
   const enabledStyle = `${defaultStyle} bg-blue-800 text-white hover:bg-blue-200 focus:bg-blue-700 active:bg-blue-800`
@@ -16,6 +16,7 @@ const ProductButton = ({ plu, description, price, onStop, functions }) => {
       onClick={() => functions.sellItem(plu, description, price)}
       disabled={onStop}
     >
+      {img ? <img src={img} className="my-1 w-[73px] h-[73px]" /> : <div className="my-1 w-[73px] h-[73px]">&nbsp;</div>}
       {description}
       <br />${price / 100}
     </button>
