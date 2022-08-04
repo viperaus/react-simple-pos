a12a4b3cdeaf88018d38c27508a356ca1791ed6e
Troy <viperaus@gmail.com>
2022-08-04 21:11:26 +1000

    Add sample images for product buttons

--- src/MockData/products.js
+++ src/MockData/products.js
@@ -1,13 +1,13 @@
 export const products = [
-  { plu: "001001", price: parseInt((Math.random() * 10000) + 1), description: "Apple", onStop: false, department: "Fruit" },
-  { plu: "001002", price: parseInt((Math.random() * 10000) + 1), description: "Orange", onStop: false, department: "Fruit" },
-  { plu: "001003", price: parseInt((Math.random() * 10000) + 1), description: "Banana", onStop: true, department: "Fruit" },
-  { plu: "001004", price: parseInt((Math.random() * 10000) + 1), description: "Pear", onStop: false, department: "Fruit" },
-  { plu: "002001", price: parseInt((Math.random() * 10000) + 1), description: "Tomato", onStop: false, department: "Veg" },
-  { plu: "002002", price: parseInt((Math.random() * 10000) + 1), description: "Lettuce", onStop: true, department: "Veg" },
-  { plu: "002003", price: parseInt((Math.random() * 10000) + 1), description: "Capsicum", onStop: false, department: "Veg" },
-  { plu: "002004", price: parseInt((Math.random() * 10000) + 1), description: "Mushroom", onStop: false, department: "Veg" },
-  { plu: "002005", price: parseInt((Math.random() * 10000) + 1), description: "Onion", onStop: false, department: "Veg" },
+  { plu: "001001", price: parseInt((Math.random() * 1000) + 1), description: "Apple", onStop: false, department: "Fruit", img:'/images/buttons/apples-fuji.gif' },
+  { plu: "001002", price: parseInt((Math.random() * 1000) + 1), description: "Orange", onStop: false, department: "Fruit", img:'/images/buttons/orange-naval.gif' },
+  { plu: "001003", price: parseInt((Math.random() * 1000) + 1), description: "Banana", onStop: true, department: "Fruit", img:'/images/buttons/bananas-large.gif' },
+  { plu: "001004", price: parseInt((Math.random() * 1000) + 1), description: "Pear", onStop: false, department: "Fruit", img:'/images/buttons/pear-packham.gif' },
+  { plu: "002001", price: parseInt((Math.random() * 1000) + 1), description: "Tomato", onStop: false, department: "Veg", img:'/images/buttons/tomatoes-truss.gif' },
+  { plu: "002002", price: parseInt((Math.random() * 1000) + 1), description: "Lettuce", onStop: true, department: "Veg", img:'/images/buttons/lettuce-iceberg.gif' },
+  { plu: "002003", price: parseInt((Math.random() * 1000) + 1), description: "Capsicum", onStop: false, department: "Veg", img:'/images/buttons/capsicum-green.gif' },
+  { plu: "002004", price: parseInt((Math.random() * 1000) + 1), description: "Mushroom", onStop: false, department: "Veg", img:'/images/buttons/mushroom-button.gif' },
+  { plu: "002005", price: parseInt((Math.random() * 1000) + 1), description: "Onion", onStop: false, department: "Veg", img:'/images/buttons/onions-brown.gif' },
 ]
 
 export const departments = [
